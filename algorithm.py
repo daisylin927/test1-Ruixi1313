b7813cfe10462f2c0bc14b713bcd56f95aff23db
daisylin927 <131406435+daisylin927@users.noreply.github.com>
2025-10-20 02:52:41 -0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,13 @@
 
 
+def sort_numbers(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+if __name__ == "__main__":
+    print(sort_numbers([5, 3, 8, 1, 2]))
