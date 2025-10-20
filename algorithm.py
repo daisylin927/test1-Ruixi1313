4dd97887d565773886011a822d559c6e94527174
daisylin927 <131406435+daisylin927@users.noreply.github.com>
2025-10-20 02:41:34 -0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,14 @@
 
-# Assignment: Greedy Algorithm (Individual)
-# Problem: 
+def sort_numbers(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+if __name__ == "__main__":
+    print(sort_numbers([5, 3, 8, 1, 2]))
 
 
